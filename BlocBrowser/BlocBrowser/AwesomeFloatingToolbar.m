52527cf1190ab227ef7786be77aa6caa7b8ced63
jordanjohnson111 <jordanjohnson111@gmail.com>
2016-08-23 13:31:38 -0500

    button actions not working yet

--- BlocBrowser/BlocBrowser/AwesomeFloatingToolbar.m
+++ BlocBrowser/BlocBrowser/AwesomeFloatingToolbar.m
@@ -7,6 +7,13 @@
 //
 
 #import "AwesomeFloatingToolbar.h"
+#import <WebKit/Webkit.h>
+
+#define kWebBrowserBackString NSLocalizedString(@"Back", @"Back command")
+#define kWebBrowserForwardString NSLocalizedString(@"Forward", @"Forward command")
+#define kWebBrowserStopString NSLocalizedString(@"Stop", @"Stop command")
+#define kWebBrowserRefreshString NSLocalizedString(@"Refresh", @"Reload command")
+
 
 @interface AwesomeFloatingToolbar ()
 
@@ -20,66 +27,12 @@
 @property (nonatomic, strong) UILongPressGestureRecognizer *longPressGesture;
 @property (nonatomic, strong) NSArray *buttons;
 @property (nonatomic, strong) UIButton *button;
+@property (nonatomic, strong) WKWebView *webView;
 
 @end
 
 @implementation AwesomeFloatingToolbar
 
-//- (instancetype) initWithFourTitles:(NSArray *)titles {
-//    // First, call the superclass (UIView)'s initializer, to make sure we do all that setup first.
-//    self = [super init];
-//    
-//    if (self) {
-//        
-//        // Save the titles, and set the 4 colors
-//        self.currentTitles = titles;
-//        self.colors = @[[UIColor colorWithRed:199/255.0 green:158/255.0 blue:203/255.0 alpha:1],
-//                        [UIColor colorWithRed:255/255.0 green:105/255.0 blue:97/255.0 alpha:1],
-//                        [UIColor colorWithRed:222/255.0 green:165/255.0 blue:164/255.0 alpha:1],
-//                        [UIColor colorWithRed:255/255.0 green:179/255.0 blue:71/255.0 alpha:1]];
-//        
-//        NSMutableArray *labelsArray = [[NSMutableArray alloc] initWithCapacity:4];
-//        
-//        // Make the 4 labels
-//        for (NSString *currentTitle in self.currentTitles) {
-//            UILabel *label = [[UILabel alloc] init];
-//            label.userInteractionEnabled = NO;
-//            label.alpha = 0.25;
-//            
-//            NSUInteger currentTitleIndex = [self.currentTitles indexOfObject:currentTitle]; // 0 through 3
-//            NSString *titleForThisLabel = [self.currentTitles objectAtIndex:currentTitleIndex];
-//            
-//            label.textAlignment = NSTextAlignmentCenter;
-//            label.font = [UIFont systemFontOfSize:10];
-//            label.text = titleForThisLabel;
-//            label.textColor = [UIColor whiteColor];
-//            
-//            [labelsArray addObject:label];
-//        }
-//        
-//        self.labels = labelsArray;
-//        
-//        [self setColorsForLabels];
-//        
-//        for (UILabel *thisLabel in self.labels) {
-//            [self addSubview:thisLabel];
-//        }
-//        
-//        self.tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapFired:)];
-//        [self addGestureRecognizer:self.tapGesture];
-//        
-//        self.panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panFired:)];
-//        [self addGestureRecognizer:self.panGesture];
-//        
-//        self.pinchGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinchFired:)];
-//        [self addGestureRecognizer:self.pinchGesture];
-//        
-//        self.longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressFired:)];
-//        [self addGestureRecognizer:self.longPressGesture];
-//    }
-//    
-//    return self;
-//}
 
 - (instancetype) initWithFourTitles:(NSArray *)titles {
     // First, call the superclass (UIView)'s initializer, to make sure we do all that setup first.
@@ -100,8 +53,7 @@
         for (NSString *currentTitle in self.currentTitles) {
             
             //create button
-            UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom]; //fix this not the way to initialize
-            //UILabel *label = [[UILabel alloc] init];
+            UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
             button.userInteractionEnabled = NO;
             button.alpha = 1;
             
@@ -121,6 +73,26 @@
             //button.tintColor = [UIColor whiteColor];
             [button setTintColor:[UIColor whiteColor]];
             
+            //define button actions
+//            NSString *actionForThisButton = [[NSString alloc] init];
+//            if ([button.titleLabel isEqual:kWebBrowserBackString]) {
+//                actionForThisButton = [self.webView goBack];
+//            } else if ([button.titleLabel isEqual:kWebBrowserForwardString]) {
+//                actionForThisButton = [self.webView goForward];
+//            } else if ([button.titleLabel isEqual:kWebBrowserStopString]) {
+//                actionForThisButton = [self.webView stopLoading];
+//            } else if ([button.titleLabel isEqual:kWebBrowserRefreshString]) {
+//                actionForThisButton = [self.webView reload];
+//            }
+            
+            //set button action
+            [button addTarget:self action:@selector(awesomeFloatingToolbar:didSelectButtonWithTitle:) forControlEvents:UIControlEventTouchUpInside];
+            //[button addTarget:self action:@selector(setEnabled:) forControlEvents:UIControlEventTouchUpInside];
+ 
+            //[button addTarget:self action:@selector([self.webView reload]:) forControlEvents:UIControlEventTouchUpInside];
+            //[button addTarget:self action:@selector(actionForThisButton:) forControlEvents:UIControlEventTouchUpInside];
+            
+            
             //add each button to the button array
             [buttonsArray addObject:button];
         }
@@ -132,9 +104,7 @@
             [self addSubview:thisButton];
         }
         
-//        self.tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapFired:)];
-//        [self addGestureRecognizer:self.tapGesture];
-       
+        //gesture actions
         self.panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panFired:)];
         [self addGestureRecognizer:self.panGesture];
         
@@ -199,11 +169,6 @@
 }
 
 - (void) longPressFired:(UILongPressGestureRecognizer *)recognizer {
-//    self.colors = @[[UIColor colorWithRed:199/255.0 green:158/255.0 blue:203/255.0 alpha:1],
-//                    [UIColor colorWithRed:255/255.0 green:105/255.0 blue:97/255.0 alpha:1],
-//                    [UIColor colorWithRed:222/255.0 green:165/255.0 blue:164/255.0 alpha:1],
-//                    [UIColor colorWithRed:255/255.0 green:179/255.0 blue:71/255.0 alpha:1]];
-
     NSMutableArray *mutableColorArray = [self.colors mutableCopy];
 
     if (recognizer.state == UIGestureRecognizerStateEnded) {
@@ -212,56 +177,13 @@
         for (NSInteger i = mutableColorArray.count-1; i > 0; i--) {
             [mutableColorArray exchangeObjectAtIndex:i withObjectAtIndex:arc4random_uniform(4)];
         }
-    
-        //self.colors = mutableColorArray;
-    
-    
-        //self = [super init];
-        
-    
-            //set new colors
-            //self.currentTitles = titles;
-            self.colors = mutableColorArray;
-        [self setColorsForButtons];
-    
-        
 
+        self.colors = mutableColorArray;
+        [self setColorsForButtons];
     }
 
 }
 
-//Below works for labels
-//- (void) layoutSubviews {
-//    // set the frames for the 4 labels
-//    
-//    for (UILabel *thisLabel in self.labels) {
-//        NSUInteger currentLabelIndex = [self.labels indexOfObject:thisLabel];
-//        
-//        CGFloat labelHeight = CGRectGetHeight(self.bounds) / 2;
-//        CGFloat labelWidth = CGRectGetWidth(self.bounds) / 2;
-//        CGFloat labelX = 0;
-//        CGFloat labelY = 0;
-//        
-//        // adjust labelX and labelY for each label
-//        if (currentLabelIndex < 2) {
-//            // 0 or 1, so on top
-//            labelY = 0;
-//        } else {
-//            // 2 or 3, so on bottom
-//            labelY = CGRectGetHeight(self.bounds) / 2;
-//        }
-//        
-//        if (currentLabelIndex % 2 == 0) { // is currentLabelIndex evenly divisible by 2?
-//            // 0 or 2, so on the left
-//            labelX = 0;
-//        } else {
-//            // 1 or 3, so on the right
-//            labelX = CGRectGetWidth(self.bounds) / 2;
-//        }
-//        
-//        thisLabel.frame = CGRectMake(labelX, labelY, labelWidth, labelHeight);
-//    }
-//}
 
 - (void) layoutSubviews {
     // set the frames for the 4 labels
@@ -327,16 +249,19 @@
     }
 }
 
-//- (void) setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title {
-//    NSUInteger index = [self.currentTitles indexOfObject:title];
-//    
-//    if (index != NSNotFound) {
-//        UIButton *button = [self.buttons objectAtIndex:index];
-//        button.userInteractionEnabled = enabled;
-//        button.alpha = enabled ? 1.0 : 0.25;
-//    }
-//}
 
+//Button Actions???
+- (void) awesomeFloatingToolbar:(AwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)button {
+    if ([button.titleLabel isEqual:kWebBrowserBackString]) {
+        [self.webView goBack];
+    } else if ([button.titleLabel isEqual:kWebBrowserForwardString]) {
+        [self.webView goForward];
+    } else if ([button.titleLabel isEqual:kWebBrowserStopString]) {
+        [self.webView stopLoading];
+    } else if ([button.titleLabel isEqual:kWebBrowserRefreshString]) {
+        [self.webView reload];
+    }
+}
 
 
 /*
